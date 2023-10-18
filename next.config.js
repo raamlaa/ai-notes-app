98154ff3c7a6744f264524a2d39aed3afa210046
Ramla Azouni <ramla7azo@gmail.com>
2023-10-18 13:11:35 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["firebasestorage.googleapis.com","oaidalleapiprodscus.blob.core.windows.net"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
